29bb1508e357b347b8aeedb54e29f97f7a6f63d3
BerniceChua <Bernice.Chua.415@gmail.com>
2017-10-19 14:37:16 -0700

    Create ActiveStateDecision1.cs & LookDecision1.cs ScriptableObjects to detect targets added during playtime instead of before the game starts.

---
+++ Assets/PluggableAI/Scripts/ActiveStateDecision1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/ActiveState1")]
+public class ActiveStateDecision1 : Decision {
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public override bool Decide(StateController controller) {
+        //bool chaseTargetIsActive = controller.chaseTarget.gameObject.activeSelf;
+        //bool chaseTargetIsActive = FindObjectOfType<Target>().gameObject.activeSelf;
+        //if (FindObjectOfType<Target>().gameObject.activeSelf != null) {
+        if (FindObjectOfType<Target>()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ Assets/PluggableAI/Scripts/LookDecision1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/Look1")]
+public class LookDecision1 : Decision {
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public override bool Decide(StateController controller) {
+        bool targetVisible = Look(controller);
+        return targetVisible;
+    }
+
+    private bool Look(StateController controller) {
+        //RaycastHit hit;
+
+        /// Will draw a green line in front of where this is looking.
+        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * controller.enemyStats.lookRange, Color.green);
+
+        Target target = FindObjectOfType<Target>();
+
+        //if (target.isActiveAndEnabled) {
+        if (target != null) {
+            controller.chaseTarget = target.transform;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}

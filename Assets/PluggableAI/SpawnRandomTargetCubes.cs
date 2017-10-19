8cd596737eaa7bb42f315245e3ea3502021bcd5c
BerniceChua <Bernice.Chua.415@gmail.com>
2017-10-18 23:02:17 -0700

    Add UI in Add-Targets-During-Playtime.unity to generate target during playtime.

---
+++ Assets/PluggableAI/SpawnRandomTargetCubes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawnRandomTargetCubes : MonoBehaviour {
+
+    [SerializeField] GameObject m_targetCube;
+
+    [SerializeField] NavMesh m_navMesh;
+
+    [SerializeField] NavMeshAgent m_navMeshAgent;
+
+    [SerializeField] Transform[] wayPointsForTargets;
+
+    //// Use this for initialization
+    //   void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public void SpawnTargetCube() {
+        Debug.Log("Clicked on the button.");
+        //m_navMeshAgent.isOnNavMesh
+        //m_targetCube;
+        GameObject spawnedTarget;
+        spawnedTarget = Instantiate(m_targetCube, wayPointsForTargets[0].position, wayPointsForTargets[0].rotation);
+    }
+
+}
